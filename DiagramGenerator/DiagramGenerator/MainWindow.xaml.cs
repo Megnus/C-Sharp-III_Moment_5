631d10caee0a367f8f0ad7a463b667b6368ae6dd
Sundström <Magnus.Sundstrom@flir.se>
2015-12-17 11:22:00 +0100

    Testing onrender a red rectangle

---
+++ DiagramGenerator/DiagramGenerator/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DiagramGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private RenderTargetBitmap buffer;
+        private DrawingVisual drawingVisual = new DrawingVisual();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+            buffer = new RenderTargetBitmap((int)Background.Width, (int)Background.Height, 96, 96, PixelFormats.Pbgra32);
+            Background.Source = buffer;
+            DrawStuff();
+        }
+
+        private void DrawStuff()
+        {
+            if (buffer == null)
+                return;
+
+            using (DrawingContext drawingContext = drawingVisual.RenderOpen())
+            {
+                drawingContext.DrawRectangle(new SolidColorBrush(Colors.Red), null, new Rect(0, 0, 10, 10));
+            }
+
+            buffer.Render(drawingVisual);
+        }
+    }
+}
